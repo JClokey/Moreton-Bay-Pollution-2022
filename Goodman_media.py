0464098377d2b7c273e59e4d7289cc7545e362a5
JClokey <joeclokey@gmail.com>
2022-12-19 14:59:35 +1000

    Update Goodman_media.py

--- Goodman_media.py
+++ Goodman_media.py
@@ -7,10 +7,6 @@ st.set_page_config(layout="wide")
 
 st.markdown("<h1 style='text-align: center; color: #49075e;'>Presence of contaminants of emerging concern in Moreton Bay during a la Niña year</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center; color: #49075e;'>Media</h2>", unsafe_allow_html=True)
-st.image("https://static.streamlit.io/examples/dice.jpg")
 
-st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
-
-with st.expander("See explanation"):
-    st.write("The chart above shows some numbers I picked for you. I rolled actual dice for these, so they're *guaranteed* to be random.")
-    st.image("https://static.streamlit.io/examples/dice.jpg")
+with st.expander("Here there be media"):
+    st.write("media files here")
